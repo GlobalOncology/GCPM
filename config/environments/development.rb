082b891dd11589ac0ea97a6737b098b2ef0d53ba
David Allen <trooper898@gmail.com>
2017-11-15 12:07:53 -0500

    don't debug assets in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Mutes assets pipeline log messages
   config.assets.quiet = true
