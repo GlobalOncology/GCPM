7d729b41d3a0e3c67f088efd52b4d6c397f10d2d
Geoff Oliver <geoff@plan8studios.com>
2017-12-13 14:22:24 -0700

    new homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @featureds = Featured.featurables
-    @projects_total = Project.fetch_all({}).count()
-    @events_total = Event.all.distinct.count()
-    @people_total = Investigator.all.distinct.count()
+    @featureds = Featured.home_featurables
+    # @projects_total = Project.fetch_all({}).count()
+    # @events_total = Event.all.distinct.count()
+    # @people_total = Investigator.all.distinct.count()
 
     gon.isMobile = browser.device.mobile?
     gon.carto_account = ENV["CARTODB_ACCOUNT"]
