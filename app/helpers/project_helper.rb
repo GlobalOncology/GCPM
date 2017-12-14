595e6f244e0fa8fd770561bef6a159388aea1364
Geoff Oliver <geoff@plan8studios.com>
2017-12-14 11:06:19 -0700

    fixed typo

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,5 +1,5 @@
 module ProjectHelper
-  def check_panding_project_user_status(project, user_id)
+  def check_pending_project_user_status(project, user_id)
     user_project = project.project_users.find_by(user_id: user_id, project_id: project.id)
     user_project.unapproved? if user_project.present?
   end
